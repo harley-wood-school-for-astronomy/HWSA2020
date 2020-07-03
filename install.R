59498b61fa351b626719d56e9903854b2b01f968
Robin Cook <RobinCook9012@gmail.com>
2020-07-03 15:58:30 +0800

    Created install.R file

---
+++ install.R
@@ -0,0 +1,8 @@
+install.packages('magicaxis')
+install.packages('pracma')
+install.packages('cooltools')
+install.packages('ellipse')
+install.packages('extraDistr')
+
+install.packages('devtools')
+devtools::install_github('obreschkow/cooltools')
