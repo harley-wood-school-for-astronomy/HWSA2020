b9edb4493680018116b35caf5344df7d1c3f7750
Robin Cook <RobinCook9012@gmail.com>
2020-07-03 17:55:33 +0800

    Update install.R

--- install.R
+++ install.R
@@ -7,7 +7,8 @@ install.packages('extraDistr')
 install.packages('devtools')
 devtools::install_github('obreschkow/cooltools')
 
-install.packages('IRkernel')
-install.packages('IRdisplay')
+install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'uuid', 'digest'))
 
-IRkernel::installspec()
+devtools::install_github('IRkernel/IRkernel')
+
+IRkernel::installspec(user = FALSE)
